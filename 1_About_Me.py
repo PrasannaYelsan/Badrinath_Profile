c9470fd23c0196951a6ff5719c8f5904cff8e3db
PrasannaYelsan <72689004+PrasannaYelsan@users.noreply.github.com>
2023-10-16 11:03:50 +0530

    Update 1_About_Me.py

--- 1_About_Me.py
+++ 1_About_Me.py
@@ -72,5 +72,6 @@ with image_col:
             switch_page("Contact_Me")
 
 with text_col:
-    image = Image.open('.\\blob\main\Images\Badri_new1.png')
-    st.image(image, caption='Badrinath Deshpande')
+    #image = Image.open('.\\blob\main\Images\Badri_new1.png')
+    #st.image(image, caption='Badrinath Deshpande')
+    pass
